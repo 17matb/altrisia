f699a6e1ccfd844fa8edafcaa2e1a9f1ecf6e8ed
17matb <147427088+17matb@users.noreply.github.com>
2025-09-05 11:09:44 +0200

    feat: provide read_root with fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from api import router
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+app.include_router(router)
