601f225e5a109d308b9cdebfb41c1d6febca4809
kirillsst <jewaor@gmail.com>
2025-09-05 12:10:15 +0200

    etz

---
+++ backend/get_supabase.py
@@ -0,0 +1,90 @@
+import psycopg2
+from sqlalchemy import create_engine, text
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+
+# Configuration de connexion (à adapter selon votre provider)
+load_dotenv()
+
+# Fetch variables
+USER = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASS")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DBNAME = os.getenv("DBNAME")
+print(HOST)
+print(USER)
+
+DATABASE_CONFIG = {
+    'host': HOST,  
+    'database': DBNAME,
+    'user': USER,
+    'password': PASSWORD,
+    'port': PORT
+}
+    
+# Création de l'engine SQLAlchemy
+engine = create_engine(
+    f"postgresql://{USER}:{PASSWORD}@"
+    f"{HOST}:{PORT}/{DBNAME}"
+)
+
+# Test de connexion
+def test_connection():
+    # Connect to the database
+    try:
+        connection = psycopg2.connect(
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT,
+            dbname=DBNAME
+        )
+        print("Connection successful!")
+        
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+        
+        # Example query
+        cursor.execute("SELECT version();")
+        result = cursor.fetchone()
+        print("Current Time:", result)
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
+    except Exception as e:
+        print(f"Failed to connect: {e}")
+test_connection()
+
+def create_tables():
+    sql = """
+    CREATE TABLE IF NOT EXISTS Categories (
+        category_id bigserial PRIMARY KEY,
+        nom text NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS Users (
+        user_id bigserial PRIMARY KEY,
+        nom text,
+        prenom text,
+        email text UNIQUE NOT NULL,
+        mdp_hash text NOT NULL,
+        date_insc timestamptz DEFAULT now()
+    );
+
+    CREATE TABLE IF NOT EXISTS Posts (
+        post_id bigserial PRIMARY KEY,
+        titre text NOT NULL,
+        description text,
+        date_crea timestamptz DEFAULT now(),
+        user_id int8 REFERENCES Users(user_id),
+        category_id int8 REFERENCES Categories(category_id),
+        media_url text
+    );
+    """
+    with engine.connect() as conn:
+            conn.execute(text(sql))
+            conn.commit()
