4680d62916dcdcd82b3e03307da04ec2bd9bb8eb
Chaima425 <chaimaabdraouf@gmail.com>
2025-09-08 14:54:10 +0200

    lire les categories

--- backend/routers/categories.py
+++ backend/routers/categories.py
@@ -8,37 +8,14 @@ router = APIRouter(
     tags=["categories"] 
 )
 
-# Dépendance DB (même méthode que users.py)
+# Dépendance DB 
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-# Ajouter une catégorie
-@router.post("/")
-def create_category(name: str, description: str = None, db: Session = Depends(get_db)):
-    category = models.Category(name=name, description=description)
-    db.add(category)
-    db.commit()
-    db.refresh(category)
-    return category
-
-
 # Lister toutes les catégories
 @router.get("/")
 def list_categories(db: Session = Depends(get_db)):
     return db.query(models.Category).all()
-
-
-# Obtenir une catégorie par ID
-@router.get("/{category_id}")
-def get_category(category_id: int, db: Session = Depends(get_db)):
-    category = db.query(models.Category).filter(models.Category.id == category_id).first()
-    if not category:
-        raise HTTPException(status_code=404, detail="Catégorie non trouvée")
-    return category
-###### 
-
