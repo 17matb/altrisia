2fcd92e73589046592ab5c8037ad8248388bf7f8
kirillsst <jewaor@gmail.com>
2025-09-09 12:14:24 +0200

    fin users.py route

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from supabase_auth import Session
 from database import models
 from fastapi import Form
@@ -8,6 +8,7 @@ from database.datasession import SessionLocal
 from dotenv import load_dotenv
 import os
 from database.hash_pass import hash_password
+from validate_email import validate_email
 
 load_dotenv()
 
@@ -28,14 +29,6 @@ def get_db():
     finally:
         db.close()
 
-# class UserCreate(BaseModel):
-#     user_id: int
-#     nom: str
-#     prenom: str
-#     email: str 
-#     mdp_hash: str
-#     date_insc: str
-
 @router.get('/')
 def read_users(db: Session = Depends(get_db)):
     return db.query(models.User).all()
@@ -50,23 +43,38 @@ def create_user(
     ):
 
     hashed_password = hash_password(password)
+    is_valid_format = validate_email(email)
 
-    new_user = models.User(
-        nom=nom,
-        prenom=prenom,
-        email=email,
-        mdp_hash=hashed_password,
-        date_insc=datetime.now(timezone.utc)
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return {"message": "Utilisateur créé", "Response": new_user}
+    if not is_valid_format:
+        return {"Email format is not valid":  email}
+    else:
+        new_user = models.User(
+            nom=nom,
+            prenom=prenom,
+            email=email,
+            mdp_hash=hashed_password,
+            date_insc=datetime.now(timezone.utc)
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return {"message": "Utilisateur créé", "Response": new_user}
 
 @router.get("/{user_id}")
-def get_user(user_id: int):
-    return {"message": f"Infos utilisateur {user_id}"}
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.user_id == id).all()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
+    return user
 
 @router.post("/login")
-def login(credentials: dict):
-    return {"message": "Connexion réussie", "token": "fake-jwt-token"}
+def login(user_email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_email).all()
+    hashed_password = hash_password(password)
+    user_password = db.query(models.User).filter(models.User.mdp_hash == hashed_password).all()
+    if not user_password:
+        raise HTTPException(status_code=404, detail="Password not valid")
+    elif not user:
+        raise HTTPException(status_code=404, detail="Email not valid")
+    return "Bienvenue"
